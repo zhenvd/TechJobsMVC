eb7b795629cfcd545e4a54666730fadedd2bcdac
Carly Langlois <cj.langlois.cl@gmail.com>
2020-06-17 10:49:46 -0500

    change techjobs controller to home, add more styling

--- TechJobsMVC/Controllers/HomeController.cs
+++ TechJobsMVC/Controllers/HomeController.cs
@@ -11,27 +11,14 @@ namespace TechJobsMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            Dictionary<string, string> actionChoices = new Dictionary<string, string>();
+            actionChoices.Add("search", "Search");
+            actionChoices.Add("list", "List");
 
-        public IActionResult Privacy()
-        {
+            ViewBag.actions = actionChoices;
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
