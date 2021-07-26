8aceee7a2c518ea645671aa668ef158bf4d67012
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-07-26 17:59:41 -0400

    added value.Equals(null) to account for empty search in JobData under FindByColumnAndValue method.

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,5 +19,20 @@ namespace TechJobsMVC.Controllers
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if(searchTerm == "")
+            {
+                ViewBag.jobs = JobData.FindAll();
+                return Index();
+            }
+            else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                return Index();
+            }
+        }
     }
 }
